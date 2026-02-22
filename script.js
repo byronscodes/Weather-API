98789b5c62f6083aa80c292108f2f4e7cba6cd25
Byron Streit <streitb2020@gmail.com>
2026-02-21 19:00:07 -0500

    finished script.js and initialized node project

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // JavaScript file using Node.js and Express to send a GET request to OpenWeather's current weather API
+// Tested GET requests with Postman during development
 
 // Import Express and intialize Express app
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// RESTful GET Request for Weather API from zip code
+// RESTful GET Request for OpenWeather API from zip code and optional scale
 app.get('/locations/:zipcode', async (req, res) => {
     const {zipcode} = req.params;
     const {scale} = req.query || "Fahrenheit";
 
-    // Check for zip code, return 400 if not entered
+    // Check for zip code, return 400 error if not entered
     if (!zipcode) {
         return res.status(400).json({
             status: 400,
@@ -18,8 +19,8 @@ app.get('/locations/:zipcode', async (req, res) => {
         });
     }
 
-    // Check for valid scale, return 400 if not valid
-    if (scale != "Fahrenheit" || scale != "Celsius") {
+    // Check for valid scale, return 400 error if not valid
+    if (scale != "Fahrenheit" && scale != "Celsius") {
         return res.status(400).json({
             status: 400,
             error: "Invalid scale entered"
@@ -27,17 +28,52 @@ app.get('/locations/:zipcode', async (req, res) => {
     }
 
     // Converts scale to OpenWeather API preferred parameter name
+    let units;
     if (scale == "Fahrenheit") {
-        scale = "imperial";
+        units = "imperial";
     }
     else {
-        scale = "metric"
+        units = "metric";
     }
 
     // Create the API URL
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=${scale}&appid=${process.env.API_KEY}`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=${units}&appid=${process.env.API_KEY}`
 
+    // Use try...catch block for catching errors when connecting with OpenWeather API
+    try {
+        const response = await fetch(apiUrl);
 
+        // Check if response is successful
+        if (!response.ok) {
+            return res.status(response.status).json({
+                status: response.status,
+                error: `OpenWeather API failed: ${response.statusText}`
+            });
+        }
 
+        // Convert successful response to JSON
+        const data = await response.json();
 
+        // Pluck wanted key
+        const {temp} = data;
+
+        // Return clean data and original scale
+        return res.status(200).json({
+            "temperature": temp,
+            "scale": scale
+        });
+
+    }
+    catch (error) {
+        // Return error encountered
+        return res.status(500).json({
+            status: 500,
+            error: `Internal server error: ${error.message}`
+        });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/locations/24060`)
 });
